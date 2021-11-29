40305f81a637188926dde43fc815c1b73f24c0ba
Nathan <nathantookeva@gmail.com>
2021-11-28 22:00:52 -0500

    elixir

--- lib/koans/06_lists.ex
+++ lib/koans/06_lists.ex
@@ -4,39 +4,39 @@ defmodule Lists do
   @intro "Lists"
 
   koan "We can see what is ahead" do
-    assert List.first([1, 2, 3]) == ___
+    assert List.first([1, 2, 3]) == 1
   end
 
   koan "Checking what's trailing is also simple" do
-    assert List.last([1, 2, 3]) == ___
+    assert List.last([1, 2, 3]) == 3
   end
 
   koan "Lists can store anything you throw at them" do
-    assert [1, 2] ++ [:a, "b"] == ___
+    assert [1, 2] ++ [:a, "b"] == [1,2,:a,"b"]
   end
 
   koan "Things can evolve" do
-    assert [1, 2, 3] -- [3] == ___
+    assert [1, 2, 3] -- [3] == [1,2]
   end
 
   koan "Evolution can have different forms" do
-    assert List.delete([:a, :b, :c], :b) == ___
+    assert List.delete([:a, :b, :c], :b) == [:a, :c]
   end
 
   koan "Precision is also valued" do
-    assert List.delete_at([:a, :b, :c], 2) == ___
+    assert List.delete_at([:a, :b, :c], 2) == [:a,:b]
   end
 
   koan "Replication is also possible" do
-    assert List.duplicate("life", 3) == ___
+    assert List.duplicate("life", 3) == ["life", "life", "life"]
   end
 
   koan "Sometimes leveling the playing field is desired" do
-    assert List.flatten([1, [2, 3], 4, [5]]) == ___
+    assert List.flatten([1, [2, 3], 4, [5]]) == [1,2,3,4,5]
   end
 
   koan "Order can also be specified for new members" do
-    assert List.insert_at([1, 2, 3], 1, 4) == ___
+    assert List.insert_at([1, 2, 3], 1, 4) == [1,4,2,3]
   end
 
   koan "We can replace things at specified positions" do
